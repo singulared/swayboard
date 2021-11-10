80b1ddfb638e84cc63c3ff2a46f7e85bd5c8c3e1
Belousov Maksim <lib.bmw@gmail.com>
2021-11-11 02:19:46 +0300

    Update swayipc to beta3

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ async fn main() -> Fallible<()> {
     tracing::subscriber::set_global_default(subscriber)
         .expect("setting tracing default failed");
     info!("test");
-    let manager = LayoutManager::new("1:1:AT_Translated_Set_2_keyboard".to_owned()).await?;
+    // let manager = LayoutManager::new("1:1:AT_Translated_Set_2_keyboard".to_owned()).await?;
+    let manager = LayoutManager::new("4152:5658:SteelSeries_SteelSeries_Apex_3".to_owned()).await?;
     let mut ipc = Connection::new().await?;
     dbg!(LayoutManager::keyboards(&mut ipc).await?);
     dbg!(
